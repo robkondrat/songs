8f8351a95df24948a9eafd9662186dfba427c172
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-01-18 19:20:55 -0600

    select button re renders components with correct selected song

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -19,5 +19,5 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
 export default combineReducers({
   songs: songsReducer,
-  selectedSongKey: selectedSongReducer
+  selectedSong: selectedSongReducer
 });
